3eaec16ff91af6fac2bd8848efc332c3d768fe02
manas-anarov <microsoft.kg@mail.ru>
2019-09-01 13:21:04 +0600

    Logout

---
+++ 2/myaccount/urls.py
@@ -0,0 +1,11 @@
+from django.urls import path
+
+from django.conf.urls import include, url
+from django.contrib.auth import views as auth_views
+
+from . import views
+
+urlpatterns = [
+	url(r'^login/$', auth_views.LoginView.as_view(template_name='myaccount/login.html')),
+	url(r'^logout/$', auth_views.LogoutView.as_view(template_name='myaccount/logout.html'), name='logout'),
+]
