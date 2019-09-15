20314c55ee60eae1caf091edf37bc0fc92bc0cf2
manas-anarov <microsoft.kg@mail.ru>
2019-09-15 20:34:19 +0600

    Delete Post

---
+++ 10/kungfu_blog/urls.py
@@ -0,0 +1,13 @@
+
+from django.contrib import admin
+from django.urls import path
+
+from django.conf.urls import include, url
+
+urlpatterns = [
+    path('admin/', admin.site.urls),
+
+    url(r'^post/', include(('mypost.urls', 'mypost'), namespace='mypost')),
+    url(r'^profile/', include(('myprofile.urls', 'myprofile'), namespace='myprofile')),
+	url(r'^account/', include(('myaccount.urls', 'myaccount'), namespace='myaccount')),
+]
