d46be3e1a37685c28db5b0ce55922af0e77cc6c5
manas-anarov <microsoft.kg@mail.ru>
2019-09-18 17:12:21 +0600

    Api category list

---
+++ 17/myprofile/forms.py
@@ -0,0 +1,15 @@
+from django import forms
+from mypost.models import Post
+
+
+class ListForm(forms.ModelForm):
+	class Meta:
+		model = Post
+		exclude = [""]
+
+
+
+class UpdateForm(forms.ModelForm):
+	class Meta:
+		model = Post
+		exclude = ('author',)
---
+++ 17/restpost/urls.py
@@ -0,0 +1,10 @@
+
+from restpost import views
+from django.conf.urls import url
+
+urlpatterns = [
+	url(r'^list/$', views.PostListAPIView.as_view(), name='restpost-list'),
+	url(r'^add/$', views.AddPost.as_view(), name='restpost-add'),
+	url(r'^(?P<id>[\w-]+)/$', views.ShowPost.as_view(), name='restpost-show'),
+	url(r'^category/list/$', views.CategoryAllAPIView.as_view(), name='restpost-list-caregory'),
+]
